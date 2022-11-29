d60a7541f798d8714799c255b9e66523287ed8a4
QJanice <queenobiekwuye@gmail.com>
2022-11-29 12:39:52 -0800

    fix

---
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Author - Queen Obiekwuye
+
+def magic_calculation(a, b, c):
+    """Match bytecode provided in the task."""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
